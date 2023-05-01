6e7718a5865dc9849c50131ccbe89ad8e86291e6
M-Farag <mina.farag@icloud.com>
2023-05-01 21:39:57 +0300

    UPD Config the command line using clap Parser

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser,Debug)]
+#[command(
+    name = "Wordy",
+    version = "0.0.1",
+    about = "A simple command line tool to replace a string in a file"
+)]
+pub struct Args {
+    
+    #[arg(short='f', long)]
+    file_path: String,
+
+    #[arg(short='o', long)]
+    origin:String,
+    
+    #[arg(short='r', long)]
+    replacement:String
+}
